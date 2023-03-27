3823bfa2655d765dc399f12a62aa76e50425b649
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-03-27 16:15:49 +0800

    change logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import styles from "./index.module.scss"
 
 import { Link } from "react-router-dom"
-import logo from "../../images/logo-icon.png"
+import logoIcon from "../../images/logo-icon.png"
+import logoText from "../../images/logo-text.png"
 
 export default function Header() {
     return (
         <div className={styles.header}>
             <Link className={styles["header__link"]} to={"https://piccollage.com/"}>
                 <div className={styles["header__logo"]}>
-                    <img className={styles["header__logo-image"]} src={logo} alt="PicCollage" />
+                    <img className={styles["header__logo-icon"]} src={logoIcon} alt="PicCollage" />
+                    <img className={styles["header__logo-text"]} src={logoText} alt="PicCollage" />
                 </div>
             </Link>
         </div>
